49d322b0035dfd939309cb25492993b14c833524
bifos <jon89071@gmail.com>
2022-10-09 23:08:17 +0900

    :construction: start on spring boot with kotlin

---
+++ server-spring-boot-kotlin/src/main/kotlin/com/bifos/tradingengine/TradingEngineApplication.kt
@@ -0,0 +1,11 @@
+package com.bifos.tradingengine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TradingEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<TradingEngineApplication>(*args)
+}
